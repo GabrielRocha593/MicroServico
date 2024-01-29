70da6fc22d5b910de23f3887ef73cc4d0adc28c7
Gabriel Rocha Salomão <62037704+GabrielRocha593@users.noreply.github.com>
2024-01-29 16:42:59 -0300

    Inicial

---
+++ Consumidor/Data/Contexto/Context.cs
@@ -0,0 +1,20 @@
+﻿using Consumidor.Data.Modelo;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Consumidor.Data.Contexto
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> opts) : base(opts)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+        public DbSet<UsuarioDB> Noticia { get; set; }
+    }
+}
